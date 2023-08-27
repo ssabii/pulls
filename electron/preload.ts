35064dfa890c0d7777564ff38940bac97491b816
ssabii <fhy1930@gmail.com>
2023-08-27 19:48:09 +0900

    feat: api contextBridge 노출

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,19 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { contextBridge } from 'electron';
+import { tokenApi } from './IpcChannel/token';
 
-const versions = {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
-  // we can also expose variables, not just functions
-}
-
-contextBridge.exposeInMainWorld('versions', versions)
-
-const dialog = {
-  openFile: () => ipcRenderer.invoke('dialog:openFile')
-}
-
-contextBridge.exposeInMainWorld('dialog', dialog)
-
-export type Versions = typeof versions
-export type Dialog = typeof dialog
+contextBridge.exposeInMainWorld('tokenApi', tokenApi)
