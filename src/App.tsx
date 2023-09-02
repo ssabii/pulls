1a0530e82af56f1ad7a898c0e37e5fdbe0aba04f
ssabii <fhy1930@gmail.com>
2023-09-03 05:06:21 +0900

    feat: nextui 예제 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import { useTheme } from 'next-themes';
 import { useState } from 'react';
-import './App.css';
+
 
 function App() {
   const [value, setValue] = useState('');
+  const { theme, setTheme } = useTheme();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -18,11 +22,21 @@ function App() {
   };
 
   return (
-    <div>
-      <input value={value} onChange={handleChange} />
-      <button onClick={getUsername}>Get Username</button>
-      <button onClick={setUsername}>Set Username</button>
-    </div>
+    <div className="h-screen p-4">
+      <div className='flex flex-col'>
+        <div>current theme is: {theme}</div>
+        <div className='flex gap-2'>
+          <Button onClick={() => setTheme('light')}>Light</Button>
+          <Button onClick={() => setTheme('dark')}>Dark</Button>
+          <Button onClick={() => setTheme('system')}>System</Button>
+        </div>
+      </div>
+      <div>
+        <Input value={value} onChange={handleChange} />
+        <Button onClick={getUsername}>Get Username</Button>
+        <Button onClick={setUsername}>Set Username</Button>
+      </div>
+    </div >
   );
 }
 
