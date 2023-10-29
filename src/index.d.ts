7feffc01ce37a5daa83843ecb57a1731ab215e1f
ssabii <fhy1930@gmail.com>
2023-10-29 22:56:44 +0900

    chore: 이전 파일 제거

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare interface Window {
-  tokenApi: import('../electron/IpcChannel/token').TokenApi;
-  storeApi: import('../electron/IpcChannel/store').StoreApi;
-  themeApi: import('../electron/IpcChannel/theme').ThemeApi;
-}
