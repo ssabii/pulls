685530eb535b64f30e8958cacfc4751992153ba0
ssabii <fhy1930@gmail.com>
2023-11-06 22:23:40 +0900

    feat: webpack 설치

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
   })
 
-  win.loadFile('index.html')
+  win.loadFile(`file://${__dirname}/index.html`)
 }
 
 app.whenReady().then(() => {
